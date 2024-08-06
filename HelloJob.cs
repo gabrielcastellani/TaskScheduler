0c45ef67463badd9e4b67134241a02e2b9b13c8b
Gabriel Castellani de Oliveira <gabrielcastellanioliveira@gmail.com>
2024-08-06 10:28:30 -0300

    Basic structure for quartz tasks

---
+++ HelloJob.cs
@@ -0,0 +1,20 @@
+﻿using Quartz;
+
+namespace TaskScheduler
+{
+    internal sealed class HelloJob : IJob
+    {
+        private readonly ILogger<HelloJob> _logger;
+
+        public HelloJob(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<HelloJob>();
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("Hello world!");
+            return Task.CompletedTask;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,25 @@
+using Quartz;
+using TaskScheduler;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+services.AddQuartz(builder =>
+{
+    var jobKey = new JobKey("HelloJob");
+    builder.AddJob<HelloJob>(options => options.WithIdentity(jobKey));
+    builder.AddTrigger(
+        options => options
+            .ForJob(jobKey)
+            .WithIdentity("HelloJob-Trigger")
+            .WithCronSchedule("0 * * ? * *"));
+});
+
+services.AddQuartzHostedService(builder =>
+{
+    builder.WaitForJobsToComplete = false;
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
